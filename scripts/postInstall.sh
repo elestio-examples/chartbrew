2af90665ef8fca1ec1fba3194eff43e7cf64f382
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-03-16 16:04:21 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -57,7 +57,12 @@ docker exec elestio-nginx nginx -s reload;
 
 sleep 250s;
 
-
+curl https://${DOMAIN}/api/app/users \
+  -H 'accept: application/json' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'content-type: application/json' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36' \
+  --compressed
 
 curl https://${DOMAIN}/api/user \
   -H 'accept: application/json' \
