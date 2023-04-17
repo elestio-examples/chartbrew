a9b21fcb536045a329cbfa21ee10f3d6ceed00c1
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-04-17 10:08:33 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -5,3 +5,32 @@
 
 # mkdir -p ./data
 # chown -R 1000:1000 ./data
+
+cat <<EOT > ./settings.js
+
+module.exports = {
+  port: process.env.CB_API_PORT,
+  secret: process.env.CB_SECRET,
+  client: process.env.REACT_APP_CLIENT_HOST,
+  api: process.env.CB_API_HOST,
+  adminMail: $ADMIN_EMAIL,
+  mailSettings: {
+    host: $CB_MAIL_HOST,
+    port: $CB_MAIL_PORT,
+    secure: $CB_MAIL_SECURE,
+    auth: {
+      user: process.env.CB_MAIL_USER,
+      pass: process.env.CB_MAIL_PASS,
+    },
+  },
+  google: {
+    client_id: process.env.CB_GOOGLE_CLIENT_ID,
+    client_secret: process.env.CB_GOOGLE_CLIENT_SECRET,
+    redirect_url: "/google-auth",
+  },
+  teamRestricted: false,
+  signupRestricted: false,
+  chartbrewMainAPI: "https://api.chartbrew.com",
+};
+
+EOT
