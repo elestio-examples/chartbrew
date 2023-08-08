a37bedff64fa143d9f520cbe1af57fadc8182b53
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-08 19:19:04 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -6,6 +6,23 @@ set -o allexport; source .env; set +o allexport;
 # mkdir -p ./data
 # chown -R 1000:1000 ./data
 
+cat <<EOT > ./servers.json
+{
+    "Servers": {
+        "1": {
+            "Name": "local",
+            "Group": "Servers",
+            "Host": "172.17.0.1",
+            "Port": 5432,
+            "MaintenanceDB": "postgres",
+            "SSLMode": "prefer",
+            "Username": "postgres",
+            "PassFile": "/pgpass"
+        }
+    }
+}
+EOT
+
 cat <<EOT > ./settings.js
 
 module.exports = {
